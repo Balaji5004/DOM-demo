8da2775de9903adad74446d583002d705ef9f8e4
Balaji5004 <122641184+Balaji5004@users.noreply.github.com>
2023-03-13 12:20:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var res=document.getElementById("main").value;
+    console.log(res);
+    var res1=document.getElementById("Middle name").value;
+    console.log(res1);
+    var res2=document.getElementById("last name").value;
+    console.log(res2);
+    function foo(){
+    var res3=document.getElementById("email").value;
+    console.log(res3);
+    var res4=document.getElementById("password").value;
+    console.log(res4);
+    }
